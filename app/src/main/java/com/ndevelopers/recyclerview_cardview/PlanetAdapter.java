bc08785d028bd11465a3da36ebf16fd8104409b9
Didier Matos <d.matostorres@gmail.com>
2019-03-27 14:13:57 -0500

    Agregando 1 arrayList y un Contexto con sus constructores que es usado en el metodo getItemCount y onCreateViewHolder

--- app/src/main/java/com/ndevelopers/recyclerview_cardview/PlanetAdapter.java
+++ app/src/main/java/com/ndevelopers/recyclerview_cardview/PlanetAdapter.java
@@ -1,12 +1,24 @@
 package com.ndevelopers.recyclerview_cardview;
 
+import android.content.Context;
 import android.support.annotation.NonNull;
 import android.support.v7.widget.RecyclerView;
+import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.TextView;
 
+import java.util.ArrayList;
+
 public class PlanetAdapter extends RecyclerView.Adapter<PlanetAdapter.PlanetHolder>{
+
+    private Context context;
+    private ArrayList<Planet> planets;
+
+    public PlanetAdapter(Context context, ArrayList<Planet> planets) {
+        this.context = context;
+        this.planets = planets;
+    }
 
     public class PlanetHolder extends RecyclerView.ViewHolder {
 
@@ -26,7 +38,8 @@ public class PlanetAdapter extends RecyclerView.Adapter<PlanetAdapter.PlanetHold
     @NonNull
     @Override
     public PlanetHolder onCreateViewHolder(@NonNull ViewGroup viewGroup, int i) {
-        return null;
+        View view = LayoutInflater.from(context).inflate(R.layout.item_row, viewGroup, false);
+        return new PlanetHolder(view);
     }
 
     @Override
@@ -36,6 +49,6 @@ public class PlanetAdapter extends RecyclerView.Adapter<PlanetAdapter.PlanetHold
 
     @Override
     public int getItemCount() {
-        return 0;
+        return planets.size();
     }
 }
